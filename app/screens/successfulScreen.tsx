1ed0b04ac690c2af1ce1e737d5783d9d7ac7dbef
Narco Shilukukwa <geco.main@gmail.com>
2024-08-02 15:30:42 +0200

    first commit

---
+++ app/screens/successfulScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function Success() {
+  return (
+    <View>
+        <Text>Successful Screen</Text>
+    </View>
+  )
+}
+
+export default Success
---
+++ components/navigation/topNavigation.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Naviagtion() {
+  return (
+    <View>
+        <Text>Navigation</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+})
