184ea1a5bc9b70e07f2302d7d66a445b670ab21b
Ghost-cypher28 <wmbale523@gmail.com>
2024-08-08 03:48:57 +0200

    ConfrimDetails Screen

---
+++ app/screens/ConfirmBusDetailScreen .tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { View, Button } from 'react-native';
+import ConfirmDetailsModal from './../Modal/ConfirmDetailsModal';
+
+const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  
+  const passengerDetails = {
+    name: 'Tiza Mpata',
+    phone: '+260 961780810',
+    departureTime: '09:00hrs',
+    tickets: 1,
+    from: 'Lusaka',
+    to: 'Kabwe',
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button title="Show Confirm Details" onPress={() => setModalVisible(true)} />
+      <ConfirmDetailsModal 
+        visible={modalVisible} 
+        onClose={() => setModalVisible(false)} 
+        passengerDetails={passengerDetails} 
+      />
+    </View>
+  );
+};
+
+export default App;
