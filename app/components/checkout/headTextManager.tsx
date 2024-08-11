be4e2b537519476fc9599b3c191c64315419f589
unknown <narcoa47>
2024-08-12 01:33:32 +0200

    extra changes

---
+++ app/components/checkout/headTextManager.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function HeadTextManager() {
+  return (
+    <View>
+        <Text>Company Name</Text>
+        <Text>Live Location, Address Chitu Road</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
