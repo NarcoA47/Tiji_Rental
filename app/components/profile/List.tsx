71c25e0b70f6a9e901d125f5541e3e2cd6934282
Ghost-cypher28 <wmbale523@gmail.com>
2024-08-15 13:42:27 +0200

    router + divider

--- app/components/profile/List.tsx
+++ app/components/profile/List.tsx
@@ -1,42 +1,61 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import { List, MD3Colors } from 'react-native-paper';
+import { List, MD3Colors, Divider } from 'react-native-paper';
 import { Ionicons, MaterialIcons } from '@expo/vector-icons'; //
+import { Dropdown, Button } from "react-native-magnus";
+import { useNavigation } from 'expo-router';
 
 export default function ListContainer() {
+  const dropdownRef = React.createRef();
+  const navigation = useNavigation()
+  
   return (
     <ScrollView>
         <View style={styles.subheaderContainer}>
             <View style={styles.grayLine} />
         </View>
-        <View style={styles.container}>
+        <View>
             <List.Section>
                 <List.Item
+                    style={styles.container}
                     title="Settings"
+                    onPress={() => navigation.navigate('ProductView')}
                     left={() => <Ionicons name="settings-outline" size={24} color={MD3Colors.primary100} />}
                 />
+                <Divider />
                 <List.Item
+                    style={styles.container}
                     title="History"
                     left={() => <Ionicons name="time-outline" size={24} color={MD3Colors.primary100} />}
                 />
+                <Divider />
                 <List.Item
+                    style={styles.container}
                     title="Add Card"
                     left={() => <MaterialIcons name="credit-card" size={24} color={MD3Colors.primary100} />}
                 />
+                <Divider />
                 <List.Item
+                    style={styles.container}
                     title="Support"
                     left={() => <Ionicons name="help-circle-outline" size={24} color={MD3Colors.primary100} />}
                 />
+                <Divider />
                 <List.Item
+                    style={styles.container}
                     title="Share"
                     left={() => <Ionicons name="share-social-outline" size={24} color={MD3Colors.primary100} />}
                 />
-                <List.Item
-                    title="About Us"
-                    left={() => <Ionicons name="information-circle-outline" size={24} color={MD3Colors.primary100} 
-                    
-                    />}
-                />
+                <Divider />
+                <View>
+                  <List.Item
+                      style={styles.container}
+                      title="About Us"
+                      left={() => <Ionicons name="information-circle-outline" size={24} color={MD3Colors.primary100} 
+                      
+                      />}
+                  />
+                </View>
             </List.Section>
         </View>
     </ScrollView>
@@ -49,7 +68,6 @@ const styles = StyleSheet.create({
     paddingLeft: 40,
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
-    marginBottom: 50,
   },
   subheaderContainer: {
     marginVertical: 10,
