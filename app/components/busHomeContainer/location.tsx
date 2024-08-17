e25d9a1142bbcd9b8e43d095bfa1c1e9fb912df7
Ghost-cypher28 <wmbale523@gmail.com>
2024-08-17 13:40:58 +0200

    links

--- app/components/busHomeContainer/location.tsx
+++ app/components/busHomeContainer/location.tsx
@@ -1,46 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import {
-  FlatList,
-  TextInput,
-  TouchableOpacity,
-  Text,
-  StyleSheet,
-  View,
-} from 'react-native';
-// import { requestLocations } from 'store/reducers/locationReducer';
+import { FlatList, TextInput,TouchableOpacity, Text, StyleSheet, View, } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
-// import { GRAY, BLACK } from 'theme/styles/colors';
 
-const LocationSearch = props => {
+const LocationSearch = (props: { onSelectLocation: any; placeholder: any; onPress: any; }) => {
   const { onSelectLocation, placeholder, onPress } = props;
   const [searchText, setSearchText] = useState('');
   const [locations, setLocations] = useState([]);
-//   const destinations = useSelector(state => state.location.locations);
+  const destinations = useSelector(state => state.location.locations);
 
   const dispatch = useDispatch();
 
   const fetchLocations = () => {
-    // let whereToLocations = destinations.results.filter(item =>
-    //   item.where_to.toLowerCase().includes(searchText.toLowerCase()),
-    // );
-    // return setLocations(whereToLocations);
+    let whereToLocations = destinations.results.filter(item =>
+      item.where_to.toLowerCase().includes(searchText.toLowerCase()),
+    );
+    return setLocations(whereToLocations);
   };
 
   useEffect(() => {
     fetchLocations();
-    // console.log("These are the destination s",  destinations)
+    console.log("These are the destination s",  destinations)
   }, [searchText]);
 
-  const getUniqueResults = results => {
-    // if (results) {
-    //   const uniqueResults = Array.from(
-    //     new Set(results.map(a => a.where_to)),
-    //   ).map(where_to => {
-    //     return results.find(a => a.where_to === where_to);
-    //   });
-    //   return uniqueResults;
-    // }
-    // return [];
+  const getUniqueResults = (results: any[]) => {
+    if (results) {
+      const uniqueResults = Array.from(
+        new Set(results.map(a => a.where_to)),
+      ).map(where_to => {
+        return results.find(a => a.where_to === where_to);
+      });
+      return uniqueResults;
+    }
+    return [];
   };
 
   return (
@@ -58,8 +49,8 @@ const LocationSearch = props => {
         ListEmptyComponent={<Text style={styles.container}>No results</Text>}
         renderItem={({ item }) => (
           <>
-            <TouchableOpacity onPress={() => onPress()}>
-              <Text style={styles.container}>where_to</Text>
+            <TouchableOpacity onPress={() => onPress(item.where_to)}>
+              <Text style={styles.container}>{item.where_to}</Text>
             </TouchableOpacity>
           </>
         )}
--- app/components/home/changer.tsx
+++ app/components/home/changer.tsx
@@ -5,37 +5,21 @@ import { TextInput as RNTextInput } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler';
 import RNPickerSelect from 'react-native-picker-select';
 import { Searchbar } from 'react-native-paper';
-import LocationSearch from '../busHomeContainer/location';
-// import { routes } from 'routes/routes';
 
-export default function Changer() {
-
+const Changer = ({navigation, route}) => {
+    // const { search } = route.params;
     const [text, setText] = React.useState("");
     
-    // const { search } = route.params;
-
-    const handleSetLocation = () => {
-        // if (search === 'to') {
-        // navigation.navigate(routes.home, {
-        //     ...route.params,
-        //     to: data,
-        // });
-        // } else {
-        // navigation.navigate(routes.home, {
-        //     ...route.params,
-        //     from: data,
-        // });
-        // }
-    };
+    const [searchQuery, setSearchQuery] = React.useState('');
   return (
     <ScrollView>
 
         
-        <View style={{ flex: 1 }}>
-            <LocationSearch
-                onPress={handleSetLocation}
-                // placeholder={search === 'from' ? 'Current Location' : 'Destination'}
-
+        <View style={styles.container}>
+            <Searchbar
+            placeholder="Search"
+            onChangeText={setSearchQuery}
+            value={searchQuery}
             />
         </View>
         <View style={styles.container}>
@@ -75,3 +59,5 @@ const styles = StyleSheet.create({
         marginBottom: 15,
     },
 })
+
+export default Changer;
