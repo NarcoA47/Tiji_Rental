faecec2d812235f22dda40012f12c1d9174b66b3
Ghost-cypher28 <wmbale523@gmail.com>
2024-08-25 19:48:16 +0200

    Contact Resource

--- app/components/support/list.tsx
+++ app/components/support/list.tsx
@@ -4,7 +4,7 @@ import { List, MD3Colors, Divider } from 'react-native-paper';
 import { Ionicons, MaterialIcons } from '@expo/vector-icons'; //
 import { Text } from "react-native-magnus";
 import { useNavigation } from 'expo-router';
-
+import { contactDetails} from './../../services/contactResource'
 export default function SupportListContainer() {
   const dropdownRef = React.createRef();
   const navigation = useNavigation()
@@ -13,7 +13,7 @@ export default function SupportListContainer() {
     <ScrollView>
         <View>
             <Text mt="lg" letterSpacing={2} color="gray700">
-            SIMPA TECHNOLOGIES LIMITED is located in Lusaka , Zambia and is part of the transport industry. We are a new company and we envision to penetrate the transport industry by allowing our clients to book tickets for long distances from the comfort of their homes. 
+              {contactDetails.about}
             </Text>
             <Text mt="lg" letterSpacing={2} color="gray700">With the coming of COVID-19 ,we believe in diconjesting bus stations and only allowing passengers who have reserved and booked tickets to their destination to freely move in those premises. This is in view to curb the spread of COVID-19.</Text>
         </View>
---
+++ app/services/contactResource.js
@@ -0,0 +1,19 @@
+export const contactDetails = {
+    email: 'info@tijiapp.com',
+    phone: '+260979298948',
+    about: 'SIMPA TECHNOLOGIES LIMITED is located in Lusaka , Zambia and is part of the transport industry. We are a new company and we envision to penetrate the transport industry by allowing our clients to book tickets for long distances from the comfort of their homes. ',
+    socials: [
+      {
+        name: 'Facebook',
+        url: 'https://www.facebook.com/TijiTicket?mibextid=LQQJ4d',
+      },
+      {
+        name: 'Twitter',
+        url: 'https://twitter.com/tijiapp?s=21&t=Ux8gJwzLjiHlmvL4v1G6OA',
+      },
+      {
+        name: 'Instagram',
+        url: 'https://instagram.com/tiji_ticket?igshid=OGQ5ZDc2ODk2ZA==',
+      },
+    ],
+}
